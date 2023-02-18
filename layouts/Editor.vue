32c906c50b4cea3e4cde055cc293a0e5339e9fe8
rowan <rowan@g1project.net>
2023-02-19 03:23:46 +0900

    init

---
+++ layouts/Editor.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>header</header>
+  <main><slot/></main>
+  <footer>footer</footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-slate-500">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import {definePageMeta} from "#imports"
+
+definePageMeta({
+  layout: 'editor'
+})
+</script>
+
+<style scoped>
+
+</style>
