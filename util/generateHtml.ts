f42fe1fbd09dc764651753ca22284a734787e6f8
rowan <rowan@g1project.net>
2023-03-17 02:54:09 +0900

    generateHtml

---
+++ util/generateHtml.ts
@@ -0,0 +1,71 @@
+import {Node as NodeType, ResponsiveMode} from "~/model/Node"
+
+export const generateHtml = (nodes: NodeType[]): string => nodes
+    .reduce((acc, current) => {
+      const classes = generateNodeCss(current)
+      const html = current.widget?.html
+          ? `<div class="${classes}">${current.widget.html.replace(/\r|\n|\t/, '')}</div>`
+          : `<div class="${classes}">${generateHtml(current.nodes)}</div>`
+      return acc + html
+    }, '')
+
+const generateNodeCss = (node: NodeType) => (<ResponsiveMode[]>Object
+    .keys(node.layout))
+    .reduce<string>((acc, current) => {
+      let result = ''
+      if (node.layout[current].type)
+        result = result + `${current}:type-${node.layout[current].type} `
+      if (node.layout[current].direction)
+        result = result + `${current}:direction-${node.layout[current].direction} `
+      if (node.layout[current].columns)
+        result = result + `${current}:columns-${node.layout[current].columns} `
+      if (node.layout[current].gap)
+        result = result + `${current}:gap-${node.layout[current].gap} `
+      if (node.layout[current].width)
+        result = result + `${current}:width-${node.layout[current].width} `
+      else
+        result = result + `${current}:width-full `
+      if (node.layout[current].height)
+        result = result + `${current}:height-${node.layout[current].height} `
+      if (node.layout[current].maxWidth)
+        result = result + `${current}:maxWidth-${node.layout[current].maxWidth} `
+
+      if (node.layout[current].mainAxis !== undefined)
+        result = result + `${current}:mainAxis-${node.layout[current].mainAxis} `
+      else
+        result = result + `${current}:mainAxis-start `
+
+      if (node.layout[current].crossAxis !== undefined)
+        result = result + `${current}:crossAxis-${node.layout[current].crossAxis} `
+      else
+        result = result + `${current}:crossAxis-start `
+
+      if (node.layout[current].position !== undefined)
+        result = result + `${current}:position-${node.layout[current].position} `
+      else
+        result = result + `${current}:position-relative `
+
+
+      if (node.layout[current].hidden !== undefined)
+        result = result + `${current}:hidden-${node.layout[current].hidden || false} `
+
+      if (node.layout[current].paddingLeft !== undefined)
+        result = result + `${current}:padding-left-${node.layout[current].paddingLeft} `
+      if (node.layout[current].paddingTop !== undefined)
+        result = result + `${current}:padding-top-${node.layout[current].paddingTop} `
+      if (node.layout[current].paddingRight !== undefined)
+        result = result + `${current}:padding-right-${node.layout[current].paddingRight} `
+      if (node.layout[current].paddingBottom !== undefined)
+        result = result + `${current}:padding-bottom-${node.layout[current].paddingBottom} `
+
+      if (node.layout[current].left !== undefined)
+        result = result + `${current}:left-${node.layout[current].left} `
+      if (node.layout[current].top !== undefined)
+        result = result + `${current}:top-${node.layout[current].top} `
+      if (node.layout[current].right !== undefined)
+        result = result + `${current}:right-${node.layout[current].right} `
+      if (node.layout[current].bottom !== undefined)
+        result = result + `${current}:bottom-${node.layout[current].bottom} `
+
+      return acc + result
+    }, '')
--- util/util.ts
+++ util/util.ts
@@ -7,3 +7,7 @@ export const generateUniqueId = (): string => new ShortUniqueId({
   //HTML의 element에 id를 부여할 때, 맨 처음 숫자가 숫자면 에러
   dictionary: "alpha"
 })()
+
+const removeSpacing = (str: string) => str
+    .replace(/\n|\r|\t|\s\s+/gi, '')
+    .replace(/>\s+</gi, '><')
