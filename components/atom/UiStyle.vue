14418049102d449a3a43b96eb1c70ea9dbbd602d
rowan <rowan@g1project.net>
2023-04-15 02:41:01 +0900

    사용안하는 폴더 제거

--- components/atom/UiStyle.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-import {defineComponent, h} from 'vue'
-
-export default defineComponent({
-  setup(props, {attrs, slots}) {
-    return () => h("style", attrs, slots)
-  }
-})
-</script>
